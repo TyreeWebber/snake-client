2f2f077ff48a685ccf9e191f424fccfc4368ebd1
Tyree Webber <webber.tyree@gmail.com>
2022-06-08 19:50:15 -0600

    created constants.js and tested to see if emojis & html entities work in the messages

---
+++ constants.js
@@ -0,0 +1,21 @@
+const IP = '165.227.47.243';
+const PORT = 50541;
+const MOVE_UP_KEY = 'w';
+const MOVE_RIGHT_KEY = 'd';
+const MOVE_DOWN_KEY = 's';
+const MOVE_LEFT_KEY = 'a';
+const messages = {
+  '1': 'Say: om nom nom',
+  '2': 'Say: Please don\'t eat me!',
+  '3': 'Say: ⦿⥐⦿'
+};
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP_KEY,
+  MOVE_RIGHT_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_LEFT_KEY,
+  messages
+};
